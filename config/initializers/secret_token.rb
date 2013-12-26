26a30be3e6c5761bcf0b66e692fdf90a0332eeee
Lee Miller <iarkeus@gmail.com>
2013-12-25 21:00:44 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,3 @@
+# TAKE IT. TAKE IT IF YOU DARE.
+# TODO: Env or some shit.
+Blackboard::Application.config.secret_key_base = '7812333456fb31680ba7364e28148ccd3953678e6b5dc772fe3eb08906973324f3c139436acb6d1f9a60e769f6f1a09a9409f3ad94f269c3ac455afb1bcc945b'
