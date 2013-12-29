8b39db6c8f6e9d27ec68ee603e73ef56f8e515aa
Lee Miller <iarkeus@gmail.com>
2013-12-29 01:50:24 -0800

    Colorful word display.

--- app/assets/javascripts/write.js
+++ app/assets/javascripts/write.js
@@ -19,12 +19,13 @@ Controller.ready("write", function() {
 		$(this).edit();
 	});
 	
-	var Document = new (function(contentField, saveField, wordsField) {
+	var Document = new (function(contentField, saveField, wordsField, goalField) {
 		var UPDATE_DELAY = 500; // ms
 		
 		var $content;
 		var $save;
 		var $words;
+		var $goal;
 		var contentValue;
 		var timer;
 		
@@ -32,11 +33,15 @@ Controller.ready("write", function() {
 			$content = contentField;
 			$save = saveField;
 			$words = wordsField;
+			$goal = goalField;
 			$content.on("keyup paste", onChange);
+			
+			updateSaveDisplay();
+			updateWordDisplay();
 		};
 		
 		var countWords = function(words) {
-			return words.split(/\s+/).length;
+			return !words ? 0 : words.match(/[^\s]+/g).length;
 		};
 		
 		var onChange = function(event) {
@@ -71,9 +76,24 @@ Controller.ready("write", function() {
 		};
 		
 		var updateWordDisplay = function() {
-			$words.text(countWords($content.val()));
+			var numWords = countWords($content.val());
+			var color = calculateWordColor(numWords, parseInt($goal.text()));
+			$words.text(numWords).css("color", color);
+		};
+		
+		var calculateWordColor = function (words, goal) {
+			var progress = words / goal;
+			if (progress > 1) {
+				progress = 1;
+			}
+			var redProgress = progress < 0.5 ? 1 : 1 - ((progress - 0.5) * 2);
+			var greenProgress = progress > 0.5 ? 1 : progress * 2;
+			var redHex = Math.floor(redProgress * 255).toString(16);
+			var greenHex = Math.floor(greenProgress * 255).toString(16);
+			var blueHex = "00";
+			return "#" + (redHex.length == 1 ? "0" + redHex : redHex) + (greenHex.length == 1 ? "0" + greenHex : greenHex) + blueHex;
 		};
 		
 		this.initialize();
-	})($("#write-body"), $("#save-display"), $("#document-words-current"));
+	})($("#write-body"), $("#save-display"), $("#document-words-current"), $("#document-words-goal"));
 });
